8dc5b626292ff2541bc9d6826bd7c4366b885ee6
Deathblood69 <deatblood69002@gmail.com>
2026-08-20 02:21:09 +0200

    feat(mui): Replace components by mui

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,14 @@
+import {type ReactNode, StrictMode} from 'react'
+
+interface Props {
+    children: ReactNode
+}
+
+export default function RootLayout({ children }: Props) {
+    return (
+        <StrictMode>
+            {children}
+        </StrictMode>
+    )
+}
+
